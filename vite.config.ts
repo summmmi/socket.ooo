f27edc39020d9bf81c396ec85690a8ec50873880
summmmi <82376660+summmmi@users.noreply.github.com>
2025-08-17 19:13:49 +0900

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          mqtt: ['mqtt'],
+          supabase: ['@supabase/supabase-js']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
